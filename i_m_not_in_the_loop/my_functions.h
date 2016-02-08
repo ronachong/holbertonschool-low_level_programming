d71a9505d7fbb4f8bb2af57f148bc4d952c9e3d7
Rona Chong <ronachong@gmail.com>
2016-02-08 04:01:26 +0000

    Add prototype for print_number() to my_functions.h.

--- i_m_not_in_the_loop/my_functions.h
+++ i_m_not_in_the_loop/my_functions.h
@@ -4,3 +4,4 @@ void print_alphabet(void); /* prototype for print_alphabet() */
 void print_tebahpla(void); /* prototype for print_tebahpla() */
 void print_base16(void); /* prototype for print_base16() */
 void positive_or_not(int n); /* prototype for positive_or_not() */
+void print_number(int n); /* prototype for print_number() */
