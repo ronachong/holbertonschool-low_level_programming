fd41082bbb4464f27363cef68674a60c5f28ee3c
Rona Chong <ronachong@gmail.com>
2016-02-08 01:03:16 +0000

    Modify my_function.h to include prototype for print_base16() function.

--- i_m_not_in_the_loop/my_functions.h
+++ i_m_not_in_the_loop/my_functions.h
@@ -2,3 +2,5 @@ int print_char(char c); /* prototype for print_char() */
 void print_z(void); /* prototype for  print_z() */
 void print_alphabet(void); /* prototype for print_alphabet() */
 void print_tebahpla(void); /* prototype for print_tebahpla() */
+void print_base16(void); /* prototype for print_base16() */
+void positive_or_not(int n); /* prototype for positive_or_not() */
