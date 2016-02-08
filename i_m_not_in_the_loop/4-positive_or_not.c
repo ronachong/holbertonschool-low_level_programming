3403792e982abd2256f9dfcee9e80e1a329a9446
Rona Chong <ronachong@gmail.com>
2016-02-08 02:54:42 +0000

    Modified elif statements into if statements.

--- i_m_not_in_the_loop/4-positive_or_not.c
+++ i_m_not_in_the_loop/4-positive_or_not.c
@@ -4,10 +4,10 @@ void positive_or_not(int n) {
   if (n > 0) {
     print_char('P');
   }
-  elif (n == 0) {
+  if (n == 0) {
     print_char('Z');
   }
-  elif (n < 0) {
+  if (n < 0) {
     print_char('N');
   }
 }
