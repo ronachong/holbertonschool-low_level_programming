07c3aa8a1d00b6a7f084f9a4b76fe01b2cf4d8e0
Rona Chong <ronachong@gmail.com>
2016-02-12 19:10:27 -0800

    Add tests for min and max int values for input of print_number().

--- dreams_and_dedication_are_a_powerful_combination/0-main.c
+++ dreams_and_dedication_are_a_powerful_combination/0-main.c
@@ -1,16 +1,20 @@
 #include "header.h"
 
-int main (void)
+int main(void)
 {
-    print_number(98);
-    print_char('\n');
-    print_number(402);
-    print_char('\n');
-    print_number(1024);
-    print_char('\n');
-    print_number(0);
-    print_char('\n');
-    print_number(-98);
-    print_char('\n');
-    return (0);
+  print_number(98);
+  print_char('\n');
+  print_number(402);
+  print_char('\n');
+  print_number(1024);
+  print_char('\n');
+  print_number(0);
+  print_char('\n');
+  print_number(-98);
+  print_char('\n');
+  print_number(32767);
+  print_char('\n');
+  print_number(-32768);
+  print_char('\n');
+  return (0);
 }
