261d15cba447ac8f0559573a3f2a5c008959bc63
Rona Chong <ronachong@gmail.com>
2016-02-14 23:13:15 -0800

    Split print_number() into two functions.

--- dreams_and_dedication_are_a_powerful_combination/0-print_number.c
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -1,39 +1,37 @@
 #include "header.h"
 
-void print_number(int input)
-{
+void print_number(int input) {
   int number = input;
   int power = 1;
   int n_remaining = 0;
 
   /* handle negative cases */
-  if (input < 0)
-    {
+  if (input < 0) {
       print_char('-');
       input *= -1;
       number *= -1;
     }
 
   /* reduce number to first digit and find remainder */
-  while (number > 9)
-    {
+  while (number > 9) {
       power *= 10;
       number /= 10;
     }
   n_remaining = input - number*power;
 
-  /* print number reduced to one digit*/
+  /* print number reduced to one digit and call handle_remaining() */
   print_char(number + 48);
+  handle_remaining(n_remaining, power);
+}
 
+void handle_remaining(int n_remaining, int power) {
   /* test if next digit is a 0; special case */
-  if (n_remaining * 10 < power && power > 1)
-    {
+  if (n_remaining * 10 < power && power > 1) {
       print_char('0');
     }
 
   /* recursive call with remainder of number */
-  if (n_remaining > 0)
-    {
+  if (n_remaining > 0) {
       print_number(n_remaining);
     }
 }
--- dreams_and_dedication_are_a_powerful_combination/header.h
+++ dreams_and_dedication_are_a_powerful_combination/header.h
@@ -1,5 +1,6 @@
 int print_char(char c); /* prototype for print_char() */
 void print_number(int); /* prototype for print_number() */
+void handle_remaining(int n_remaining, int power);
 void print_combination_1(void); /* prototype for print_combination_1() */
 void print_combination_2(void); /* prototype for print_combination_2() */
 void print_combination_3(void); /* prototype for print_combination_3() */
