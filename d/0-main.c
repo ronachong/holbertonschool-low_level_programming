3719c9e0e31b2c15192ade8a2d1f1c0ef3e53f71
Rona Chong <ronachong@gmail.com>
2016-02-14 19:41:15 -0800

    Add script using printf() to print any integer input.

---
+++ d/0-main.c
@@ -0,0 +1,16 @@
+#include "header.h"
+
+int main(void)
+{
+  printf_number(98);
+  printf("\n");
+  printf_number(402);
+  printf("\n");
+  printf_number(1024);
+  printf("\n");
+  printf_number(0);
+  printf("\n");
+  printf_number(-98);
+  printf("\n");
+  return (0);
+}
---
+++ d/0-printf_number.c
@@ -0,0 +1,6 @@
+#include "header.h"
+
+void printf_number(int n)
+{
+  printf("%d", n);
+}
