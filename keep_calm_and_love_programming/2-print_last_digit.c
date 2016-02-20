ae23a21419cf1c03dbd7a7e79bc4bb6d3f0265b6
Rona Chong <ronachong@gmail.com>
2016-02-19 22:59:30 -0800

    Revise algorithm to use modulus and if statements only.

--- keep_calm_and_love_programming/2-print_last_digit.c
+++ keep_calm_and_love_programming/2-print_last_digit.c
@@ -4,30 +4,16 @@ int print_char(char c);
  * last digit of an integer input. */
 
 void print_last_digit(int n) {
-  int original_input = n;
-  int power = 1;
-  int reduced_value = 999;
-
-  /* handle negative cases */
-  if (n < 0) {
-    n *= -1;
-    original_input *= -1;
+  if (n == -2147483647 - 1) {
+    print_char(8 + '0');
   }
 
-  /* take each digit off input until
-   * last digit is left */
-  while (reduced_value > 9) {
-      while (n > 9) {
-	n = n/10;
-	power *= 10;
-      }
+  else if (n < 0) {
+    n *= -1;
+  }
 
-      reduced_value = original_input - n*power;
-    }
-
-  /* print reduced value once it has been
-   * reduced to last digit */
-  print_char(reduced_value + '0');
-
-    /*[[[try to handle extreme cases??]]] */
+  else {
+    n = n % 10;
+    print_char(n + '0');
+  }
 }
