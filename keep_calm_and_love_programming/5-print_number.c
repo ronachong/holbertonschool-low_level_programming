ff4b9c1fa7107e09a2c53516b0cd9536f1b0fd09
Rona Chong <ronachong@gmail.com>
2016-02-19 23:08:37 -0800

    Add code for special case of int min; if conditionals.

--- keep_calm_and_love_programming/5-print_number.c
+++ keep_calm_and_love_programming/5-print_number.c
@@ -8,31 +8,35 @@ void print_number(int n) {
   int power = 1;
   int digits = 1;
 
+  if (n == -2147483647 - 1) {
+    print_char(8 + '0');
+  }
+  
   /* handle negative cases */
-  if (n < 0) {
+  else if (n < 0) {
     print_char('-');
     n *= -1;
     og_input *= -1;
   }
 
-  /* measure power and number of digits of input */
-  while (n > 9) {
-    n = n/10;
-    power *= 10;
-    digits += 1;
-  }
+  if (n >= 0) {
+    /* measure power and number of digits of input */
+    while (n > 9) {
+      n = n/10;
+      power *= 10;
+      digits += 1;
+    }
 
-  /* restore n to original value */
-  n = og_input;
+    /* restore n to original value */
+    n = og_input;
 
-  /* reduce n to each digit and print */
-  for (; digits > 0; digits--) {
-    n = n/power;
-    print_char(n + '0');
+    /* reduce n to each digit and print */
+    for (; digits > 0; digits--) {
+      n = n/power;
+      print_char(n + '0');
 
-    n = og_input % power; /* after printing each digit, reduce n to the digits remaining */
-    power = power/10;
-  }
-
-    /*[[[[let's take care of extreme cases!!]]]]*/
+      n = og_input % power; /* after printing each digit, reduce n to the digits remaining */
+      power = power/10;
+    }
+  } 
 }
