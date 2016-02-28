fce0b2fbecae711ac8f38effa2e5233f22cfc2d4
Rona Chong <ronachong@gmail.com>
2016-02-27 20:36:49 -0800

    Use nested for loops in triangles_in_the_term.

--- i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
@@ -1,5 +1,28 @@
-int print_char(char c);
+#include <stdio.h>
+
+/* triangles_in_term prints n number of
+ * triangles with a height of h */
 
 void triangles_in_the_term(int h, int n) {
-  print_char('A');
+  int h_store = h;
+  int i;
+  int l;
+  int row = 0;
+
+  for (; n > 0; n--) {
+    for (; h > 0; h--) {
+      row += 1;
+
+      for (l = h - 1; l > 0; l--) {
+	printf(" ");
+      }
+
+      for (i = 2 * (row - 1) + 1; i > 0; i--) {
+	printf("*");
+      }
+      printf("\n");
+    }
+    h = h_store;
+    row = 0;
+  }
 }
