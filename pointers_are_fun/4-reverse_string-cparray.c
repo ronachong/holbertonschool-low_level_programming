ca0689a2279aa67dee03ba64b24f9fe98d43d349
Rona Chong <ronachong@gmail.com>
2016-03-05 02:57:50 +0000

    Copy reverse string function with error to back up file.

---
+++ pointers_are_fun/4-reverse_string-cparray.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void reverse_string(char *s) {
+  int n; /* counter for chars in s */
+  int index;
+  int max_index; /* counter for chars in s_reverse */
+  for (n = 0; s[n] != '\0'; n++) {};
+
+  /* n now represents the length of string */
+  max_index = n - 1;
+
+  /* create a new string of equal length */
+  char s_reverse[n];
+  
+  /* loop backwards thru s and assign values to s_reverse  */
+  for (index = 0; index <= max_index; index++) {
+    s_reverse[index] = s[n-1];
+    n--;
+  }
+
+  /* reset n to its original value */
+  n = max_index + 1;
+  
+  /* now loop thru s_reverse and set each char in s to the char in s_reverse */
+  for (index = 0; index <= max_index; index++) {
+    s[index] = s_reverse[index];
+  }
+}
