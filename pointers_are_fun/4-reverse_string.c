9cb9a1d190f49f708e4414206ce4cf7f7d691bfd
Rona Chong <ronachong@gmail.com>
2016-03-05 03:23:55 +0000

    Use two temp char variables to create reverse_string without using copy array.

--- pointers_are_fun/4-reverse_string.c
+++ pointers_are_fun/4-reverse_string.c
@@ -2,26 +2,22 @@
 
 void reverse_string(char *s) {
   int n; /* counter for chars in s */
-  int i = 0; /* counter for chars in s_reverse */
+  int index;
+  int max_index; /* counter for chars in s_reverse */
+  char c_a;
+  char c_b;
+
   for (n = 0; s[n] != '\0'; n++) {};
 
   /* n now represents the length of string */
-  int max_index = n - 1;
+  max_index = n - 1;
 
-  /* create a new string of equal length */
-  char s_reverse[n];
-  
-  /* loop backwards thru s and assign values to s_reverse  */
-  for (int index = 0; index <= max_index; index++) {
-    s_reverse[index] = s[n-1];
-    n--;
-  }
-
-  /* reset n to its original value */
-  n = max_index + 1;
-  
-  /* now loop thru s_reverse and set each char in s to the char in s_reverse */
-  for (int index = 0; index <= max_index; index++) {
-    s[index] = s_reverse[index];
+  /* loop through n/2 and swap characters using temp char vars */
+  for (index = 0; index <= n/2; index ++) {
+    c_a = s[index];
+    c_b = s[max_index];
+    s[index] = c_b;
+    s[max_index] = c_a;
+    max_index --;
   }
 }
