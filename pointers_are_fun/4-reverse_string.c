dff3292677994bfc239bcb773ac9d5cf3e651f8f
Rona Chong <ronachong@gmail.com>
2016-03-04 17:07:59 -0800

    Create reverse_string function using 3 for loops.

--- pointers_are_fun/4-reverse_string.c
+++ pointers_are_fun/4-reverse_string.c
@@ -1,12 +1,27 @@
-int print_char(char c);
+#include <stdio.h>
 
 void reverse_string(char *s) {
-  int n;
+  int n; /* counter for chars in s */
+  int i = 0; /* counter for chars in s_reverse */
   for (n = 0; s[n] != '\0'; n++) {};
 
   /* n now represents the length of string */
-  /* use n to print from end of char till first char in string */
-  for (; n > -1; n--) {
-    print_char(s[n]);
+  int max_index = n - 1;
+
+  /* create a new string of equal length */
+  char s_reverse[n];
+  
+  /* loop backwards thru s and assign values to s_reverse  */
+  for (int index = 0; index <= max_index; index++) {
+    s_reverse[index] = s[n-1];
+    n--;
+  }
+
+  /* reset n to its original value */
+  n = max_index + 1;
+  
+  /* now loop thru s_reverse and set each char in s to the char in s_reverse */
+  for (int index = 0; index <= max_index; index++) {
+    s[index] = s_reverse[index];
   }
 }
