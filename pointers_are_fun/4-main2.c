870c3ec6cfe14f1e734e6f965aa24e75ab5eaa3d
Rona Chong <ronachong@gmail.com>
2016-03-07 16:28:05 -0800

    Add extra case test to main file for exercise 4.

--- pointers_are_fun/4-main2.c
+++ pointers_are_fun/4-main2.c
@@ -12,5 +12,14 @@ int main(void)
   char t[11] = "Holberton!";
   reverse_string(t);
   printf("\n%s\n", t);
+
+  char u[19] = "Holberton is cool!";
+  reverse_string(u);
+  printf("\n%s\n", u);
+
+  char v[20] = "Holberton is cool?!";
+  reverse_string(v);
+  printf("\n%s\n", v);
+  
   return (0);
 }
