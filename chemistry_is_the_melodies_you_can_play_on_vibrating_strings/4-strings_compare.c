d16fbfa0cf22832c4f11a09bd435c01742ecd5f2
Rona/Electra Chong <kompeito@gmail.com>
2016-03-16 18:15:24 -0700

    Generate first iteration of algorithm, using index variable as return var.

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/4-strings_compare.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/4-strings_compare.c
@@ -1,7 +1,13 @@
 int strings_compare(char *s1, const char *s2) {
-  /* loop thru and compare each char */
+  int i;
+
+  /* loop thru and find first instance of difference */
+  for (i = 0; s1[i] == s2[i]; i++){};
+
   /* if char in s1 < s2, return negative int */
-  /* if char in s1 > s2, return psotive int */
-  /*if equal, continue without any change*/
+  if (s1[i] < s2[i]) {
+    i*= -1;
+  }
+  return i;  /* if char in s1 > s2, return positive int; if =, return 0 */
   ;
 }
