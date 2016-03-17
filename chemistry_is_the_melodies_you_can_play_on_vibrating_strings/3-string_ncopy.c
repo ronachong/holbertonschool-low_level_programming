a546c39b33787f843a9aaa2f0fac6c183702b0f9
Rona/Electra Chong <kompeito@gmail.com>
2016-03-17 01:28:32 -0700

    Remove unnecessary conditionals from string_ncopy.

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
@@ -1,37 +1,22 @@
-int find_nonnull_length(char *str);
 int find_nonnull_length2(const char *str);
 
 char *string_ncopy(char *dest, const char *src, int n) {
   int i;
-  int d_charlength = find_nonnull_length(dest);
   int s_charlength = find_nonnull_length2(src);
 
   /* replace chars in dest with chars in src, up to the
    * no. stipulated by n and not exceeding the number of
    * chars in src */
-  for (i = 0; i < n && i < s_charlength; i++) {
-    /*    if (i > s_charlength) {
-        dest[i] = '\0';
-        } */
-    dest[i] = src[i];
-  }
-
-  /* if n exceeds the length of src AND dest, replace the rest of
-   * dest with null chars */
-  if (n > s_charlength && s_charlength < d_charlength) {
-    for (; dest[i] != '\0'; i++) {
+  for (i = 0; i < n; i++) {
+    if (i >= s_charlength) {
       dest[i] = '\0';
+    } else { 
+      dest[i] = src[i];
     }
   }
 
   return dest;
 }
-
-int find_nonnull_length(char *str) {
-  int index;
-  for (index = 0; str[index] != '\0'; index++) {};
-  return index;
-  }
 
 int find_nonnull_length2(const char *str) {
   int index;
