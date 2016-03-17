fe491b8ac39dde242bd21243a7bd3d33b48d7fb6
Rona/Electra Chong <kompeito@gmail.com>
2016-03-16 21:52:22 -0700

    Revise algorithm for reverse_array to work with even lengths.

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/5-reverse_array.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/5-reverse_array.c
@@ -9,22 +9,26 @@
 
 void reverse_array(int *a, int n) {
   int i;
+  int stop;
   int val1;
   int val2;
 
-  /* find half-way pt of string */
-  /* if length is even, make adjustment for calculating halfway pt from n */
-  if (n % 2 == 0) {
-    n = n - 1;
-  }
+  /* adjust n to represent max index of array */
+  n -= 1;
 
-  for (i = 0, n = n; i < n/2; i++) {
+  /* calculate stop point based on n */
+  stop = n/2;
+  if (n % 2 == 0) {
+    stop -= 1;
+  }
+  
+  for (i = 0, n = n; i <= stop; i++) {
     /* fetch original values */
     val1 = a[i];
-    val2 = a[n - 1 - i]; /* i is used as an index decrementer for ea. loop */
+    val2 = a[n - i]; /* i is used as an index decrementer for ea. loop */
 
     /* swap values in array */
     a[i] = val2;
-    a[n - 1 - i] = val1;
+    a[n - i] = val1;
   }
 }
