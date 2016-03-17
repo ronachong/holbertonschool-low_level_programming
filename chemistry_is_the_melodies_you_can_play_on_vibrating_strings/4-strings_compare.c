4aad7629fd197cffc180d8d1e2719b8beac4cf68
Rona/Electra Chong <kompeito@gmail.com>
2016-03-16 17:46:40 -0700

    Start strings_compare function with commenting.

---
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/4-strings_compare.c
@@ -0,0 +1,7 @@
+int strings_compare(char *s1, const char *s2) {
+  /* loop thru and compare each char */
+  /* if char in s1 < s2, return negative int */
+  /* if char in s1 > s2, return psotive int */
+  /*if equal, continue without any change*/
+  ;
+}
