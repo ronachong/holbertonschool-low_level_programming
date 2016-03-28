275fdbee4cc3cf91c2cd79e81db020e562ed0720
Rona Chong <ronachong@gmail.com>
2016-03-28 15:36:36 -0700

    Add if conditional to handle edge cases and when y is less than 0, for Task 1.

--- let_s_go_deeper/1-power.c
+++ let_s_go_deeper/1-power.c
@@ -2,10 +2,16 @@
 come up with an operation that multiplies x to value y numbers of time, by calling itself
 */
 
+#include <limits.h>
+
 int power(int x, int y) {
+  if (y < 0 || x > INT_MAX/x || x < INT_MIN/x) {
+    return -1;
+  }
+  // else
   if (y > 0) {
     return x * power(x, y - 1);
   }
-  // else
+  // ELSE, y = 0;
   return 1;
 }
