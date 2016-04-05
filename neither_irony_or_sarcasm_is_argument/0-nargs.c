43b2549422a4b9f6bdb9de4a5f626a8ac687c4ad
Rona Chong <ronachong@gmail.com>
2016-04-05 02:14:09 +0000

    Add documentation to 0-nargs.c.

--- neither_irony_or_sarcasm_is_argument/0-nargs.c
+++ neither_irony_or_sarcasm_is_argument/0-nargs.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/* function prints to std. output
+   number of arguments passed to it
+   from command line, excluding the
+   command */
+
 int main(int ac, char **av) {
-  av = av;
+  av = av; /* resolves unused var error */
   printf("%d\n", ac - 1);
   
   return (0);
