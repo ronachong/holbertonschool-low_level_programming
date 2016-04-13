f6a770d923bb3af4443a4612e5a0e37914580cbc
Rona Chong <ronachong@gmail.com>
2016-04-13 04:09:18 +0000

    Add pseudocode for Task 3.

---
+++ float_like_a_butterfly_sting_like_a_bee/3-alloc_grid.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+int **alloc_grid(int width, int height) {
+  /* compute number of chars for grid */
+  /* compute size of memory to allocate */
+  /* allocate memory */
+  /* assign chars of grid to memory */
+  ;
+}
