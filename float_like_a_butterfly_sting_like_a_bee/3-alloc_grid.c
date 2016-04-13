f825bdf985d76ecda659b68c80a5ef86860572be
Rona Chong <ronachong@gmail.com>
2016-04-13 09:52:46 +0000

    Iterate alloc_grid to only use malloc for ints.

--- float_like_a_butterfly_sting_like_a_bee/3-alloc_grid.c
+++ float_like_a_butterfly_sting_like_a_bee/3-alloc_grid.c
@@ -5,21 +5,11 @@ int **alloc_grid(int width, int height) {
   /* int_grid is a 2-dimensional grid composed of arrays for rows
    * and ints within arrays to form columns */
   /* practically speaking, int_grid is an array of pointers to arrays */
-  /* memory to allocate must be calculated for both pointers to the arrays
-   * (to be stored in int_grid) and for the ints in the arrays themselves */
+  /* memory to allocate must be calculated for ints in the arrays themselves, but
+   * not necessarily the pointers to the arrays. */
   int i;
   int j;
   
-  /* allocate writable memory for pointers to row arrays; store
-   * pointer to allocated memory in int_grid */
-  /* actually... do we really have to allocate memory, or can we
-   * just create int_grid using read-only memory?
-   * the only thing we need to write to is inside the actual arrays themselves... */
-  int_grid = malloc(sizeof(int*)*height);
-
-  /* alt ver would be:
-     int_grid[height]; */
-
   /* allocate space to host arrays of ints; assign addresses
    * as values for int_grid */
   for (i = 0; i < height; i++) {
@@ -29,7 +19,5 @@ int **alloc_grid(int width, int height) {
       int_grid[i][j] == 0;
     }
   }
-
-  
-  }
+  return int_grid;
 }
