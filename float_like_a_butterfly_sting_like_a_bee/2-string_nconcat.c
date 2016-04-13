d15471b58b730deef12515a3145135c770028ac1
Rona Chong <ronachong@gmail.com>
2016-04-13 02:08:55 +0000

    Adapt code from previous task to fulfill Task 2.

--- float_like_a_butterfly_sting_like_a_bee/2-string_nconcat.c
+++ float_like_a_butterfly_sting_like_a_bee/2-string_nconcat.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *string_concat(char *s1, char *s2) {
+char *string_nconcat(char *s1, char *s2, int n) {
   char *mem; /* initialize var to eventually hold concat'd strings */
   int i;
   int j;
 
-  /* add the sizes of s1 & s2, then use malloc
+  /* add the sizes of s1 & n, then use malloc
    * to allocate mem of equal size; assign pointer
    * to memory to var the mem */
-  mem = malloc(sizeof(s1) + sizeof(s2));
+  mem = malloc(sizeof(s1) + sizeof(n));
 
   /* if malloc failed to allocate anything, return NULL */
   if (mem == NULL) {
     return NULL;
   }
 
-  /* loop through s1 and assign chars to allocated memory */
+  /* loop through all of s1 & assign chars to allocated memory */
   for (i = 0; s1[i] != '\0'; i++) {
     mem[i] = s1[i];
   }
 
-  /* loop through s2 and assign chars to allocated memory */
-  for (j = 0; s2[j] != '\0'; j++, i++) {
+  /* loop through s2 up to s2[n] & assign chars to allocated memory */
+  for (j = 0; s2[j] != s2[n]; j++, i++) {
     mem[i] = s2[j];
   }
 
