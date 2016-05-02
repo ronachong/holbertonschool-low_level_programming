f05d2c7c06a1fc91dd00ac0e2a3e3984bd006102
Rona Chong <ronachong@gmail.com>
2016-05-02 06:38:30 +0000

    Add malloc for outer array.

--- float_like_a_butterfly_sting_like_a_bee/3-alloc_grid.c
+++ float_like_a_butterfly_sting_like_a_bee/3-alloc_grid.c
@@ -1,15 +1,25 @@
+#include <stdio.h>
 #include <stdlib.h>
+
+/* alloc_grid() creates a 2D grid of ints according to the following steps:
+   allocate memory for the outer array of a 2D array, based on height;
+   loop through the # of inner arrays and allocates memory for each inner array, based on width;
+   assign the pointer to each inner array as an element of the outer array;
+   initilize each element in the inner arrays to 0.
+*/
 
 int **alloc_grid(int width, int height) {
   int **int_grid;
   /* int_grid is a 2-dimensional grid composed of arrays for rows
    * and ints within arrays to form columns */
   /* practically speaking, int_grid is an array of pointers to arrays */
-  /* memory to allocate must be calculated for ints in the arrays themselves, but
-   * not necessarily the pointers to the arrays. */
+
   int i;
   int j;
   
+  /* allocate memory for the outer array of a 2D array, based on height; */
+  int_grid = malloc(sizeof(int *)*height);
+
   /* allocate space to host arrays of ints; assign addresses
    * as values for int_grid */
   for (i = 0; i < height; i++) {
