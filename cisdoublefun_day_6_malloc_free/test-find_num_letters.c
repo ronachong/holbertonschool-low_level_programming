f79a9d4ab410f59fe77fb18666f69f75ecf03bd5
Rona Chong <ronachong@gmail.com>
2016-05-03 20:52:35 +0000

    Update code for find_num_letters such that num_letters is initialized at 0.

--- cisdoublefun_day_6_malloc_free/test-find_num_letters.c
+++ cisdoublefun_day_6_malloc_free/test-find_num_letters.c
@@ -5,9 +5,11 @@ int find_letters_in_selected_word(char *s, int start_position) {
   int num_letters;
 
   /* loop thru string and increment up for each letter */
-  for (i = start_position; s[i] != '\0'; i++) {
+  for (i = start_position, num_letters = 0; s[i] != '\0'; i++) {
     if ((s[i] >= '0' && s[i] <=9) || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z')) {
       num_letters++;
+      printf("The letter just counted is %c\n", s[i]);
+      printf("num_letters so far is %d\n", num_letters);
     }
     if (s[i+1] < '0' || (s[i+1] > '9' && s[i+1] < 'A') || (s[i+1] > 'Z' && s[i+1] < 'a') || s[i+1] > 'z') {
       break;
@@ -18,5 +20,7 @@ int find_letters_in_selected_word(char *s, int start_position) {
 
 int main(void) {
   printf("The first word in 'This is a test. #test' has %d letters.\n", find_letters_in_selected_word("This is a test. #test", 0));
+  printf("The second word in 'This is a test. #test' has %d letters.\n", find_letters_in_selected_word("This is a test. #test", 4));
+
   return(0);
 }
