e3646dfa76b8b0338e92d20496f7eee63a039030
Rona Chong <ronachong@gmail.com>
2016-05-03 20:40:42 +0000

    Test helper function to find num of letters in word for strings_to_words function.

---
+++ cisdoublefun_day_6_malloc_free/test-find_num_letters.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int find_letters_in_selected_word(char *s, int start_position) {
+  int i;
+  int num_letters;
+
+  /* loop thru string and increment up for each letter */
+  for (i = start_position; s[i] != '\0'; i++) {
+    if ((s[i] >= '0' && s[i] <=9) || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z')) {
+      num_letters++;
+    }
+    if (s[i+1] < '0' || (s[i+1] > '9' && s[i+1] < 'A') || (s[i+1] > 'Z' && s[i+1] < 'a') || s[i+1] > 'z') {
+      break;
+    }
+  }
+  return num_letters;
+}
+
+int main(void) {
+  printf("The first word in 'This is a test. #test' has %d letters.\n", find_letters_in_selected_word("This is a test. #test", 0));
+  return(0);
+}
