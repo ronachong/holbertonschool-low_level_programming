57a6952d5a10ef646f5ef32744e7b9bd7dcf00ab
Rona Chong <ronachong@gmail.com>
2016-05-04 03:08:25 +0000

    Copy working code with printf tests to file for reference.

---
+++ cisdoublefun_day_6_malloc_free/3-string_to_words-wtests.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int find_num_words(char *s);
+char **assign_letters(char *s, int start_position, int current_index, int words_in_array, char **outer_array);
+
+char **string_to_words(char *s) {
+  /* init outer array of pointers */
+  char **outer_array; /* for some reason, geeksforgeeks exp. does not init as ** - ask Alex? */
+  int words_in_array;
+
+  words_in_array = find_num_words(s);
+ 
+  /* allocate memory for outer array */
+  outer_array = malloc(sizeof(char *)*words_in_array);
+  if (outer_array == NULL) {
+    fprintf(stderr, "Not enough memory left!\n");
+    free(outer_array);
+    return (NULL);
+  }
+
+  /* allocate memory for each inner array;
+   * assign pointers to inner arrays as elements of outer arrays;
+   * and assign word strings to allocated memory for each inner array */
+  /* return array of pointers to word strings */
+  return assign_letters(s, 0, 0, words_in_array, outer_array);
+}
+
+
+int find_num_words(char *s) {
+  int i;
+  int num_words;
+
+  /* loop thru string and increment up each time a non-alphanum char follows alphanum char */
+  for (i = 0, num_words = 0; s[i] != '\0'; i++) {
+    if ((s[i] >= '0' && s[i] <=9) || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z')) {
+      if (s[i+1] < '0' || (s[i+1] > '9' && s[i+1] < 'A') || (s[i+1] > 'Z' && s[i+1] < 'a') || s[i+1] > 'z') {
+        num_words++;
+      }
+    }
+  }
+  return num_words;
+}
+
+
+char **assign_letters(char *s, int start_position, int current_index, int words_in_array, char **outer_array) {
+  int i;
+  int j;
+  int num_letters;
+  int num_nonletters;
+  
+  /* base case: index == words_in_array, i.e. all words have been processed; return array */
+  if (current_index == words_in_array) {
+    return outer_array;
+  }
+
+  /* ELSE */
+  /* count chars in current word */
+  for (i = start_position, num_letters = 0, num_nonletters = 0; s[i] != '\0'; i++) {
+    if ((s[i] >= '0' && s[i] <= '9') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z')) {
+      num_letters++;
+    } else {
+      num_nonletters++;
+      if ((s[i+1] >= '0' && s[i+1] <= '9') ||
+	  (s[i+1] >= 'A' && s[i+1] <= 'Z') ||
+	  (s[i+1] >= 'a' && s[i+1] <= 'z')) {
+	break;
+      }
+    }
+    
+    /*if (s[i+1] < '0' || (s[i+1] > '9' && s[i+1] < 'A') || (s[i+1] > 'Z' && s[i+1] < 'a') || s[i+1] > 'z') {
+      break;
+    }*/
+  }
+
+  /* allocate memory for chars in word and assign ptr to memory to outer array */
+  outer_array[current_index] = malloc(sizeof(char)*num_letters);
+  if (outer_array[current_index] == NULL) {
+    fprintf(stderr, "Malloc for outer_array[current_index] did not work.\n");
+    free(outer_array);
+  }
+  else {
+    printf("Malloc should have worked.\n");
+    printf("current_index is %d. words_in_array is %d. num_letters is %d. num_nonletters is %d.\n", current_index, words_in_array, num_letters, num_nonletters);
+    printf("outer_array[current_index] = %p\n", outer_array[current_index]);
+  }
+
+  /* assign letters from word in string to allocated mem */
+  for (i = start_position, j = 0; j < num_letters; i++, j++) {
+    printf("j is %d, s[i] is %c\n", j, s[i]);
+    outer_array[current_index][j] = s[i];
+  }
+
+  /* recursive call */
+  return assign_letters(s, i + num_nonletters, current_index + 1, words_in_array, outer_array);
+}
