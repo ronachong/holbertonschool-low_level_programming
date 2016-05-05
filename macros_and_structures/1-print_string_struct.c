80919c6aad75f5384c3618061ec4373c5bc27193
Rona Chong <ronachong@gmail.com>
2016-05-05 23:56:25 +0000

    Iterate onto print_string_struct and helper functions.

--- macros_and_structures/1-print_string_struct.c
+++ macros_and_structures/1-print_string_struct.c
@@ -9,20 +9,38 @@ void print_number(int);
 
 void print_string_struct(struct String *str) {
   print_string(str->str);
+  print_char(',');
+  print_char(' ');
   print_number(str->length);
+  print_char('\n');
 }
 
 
 /* print_string loops through a given string and prints each char in string, using
    print_char */
 
-void print_string(char *) {
-  ;
+void print_string(char *s) {
+  int i;
+
+  for (i = 0; s[i] != '\0'; i++) {
+    print_char(s[i]);
+  }
 }
 
 /* print_number takes in an int as an input, finds each first digit and prints its char
    equivalent, till the whole int is printed */
 
-void print_number(int) {
-  ;
+void print_number(int n) {
+  int length;
+
+  length = 1;
+  while (n > 9) {
+    n /= 10;
+    length++;
+  }
+
+  print_char(n + '0');
+  
+  if (length > 1) {
+    print_number(n*10
 }
