fef64ea1f4beac58ad18b35711f8e25f2a15c235
Rona Chong <ronachong@gmail.com>
2016-05-06 04:45:14 +0000

    Use two calls to free to create free_string_struct.

---
+++ macros_and_structures/3-free_string_struct.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "str_struct.h"
+
+/* free_string_struct uses free to free the memory
+   pointed to by the given pointer */
+
+void free_string_struct(struct String *str) {
+  free(str);
+  free(str -> str);
+}
