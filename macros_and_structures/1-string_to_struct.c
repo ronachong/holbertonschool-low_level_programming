42384be5484d73fc646d182f832734e96cef99cf
Rona Chong <ronachong@gmail.com>
2016-05-06 04:41:56 +0000

    Create helper function copy_string and assign copy of input string rather than input itself to struct.

--- macros_and_structures/1-string_to_struct.c
+++ macros_and_structures/1-string_to_struct.c
@@ -3,6 +3,7 @@
 
 int print_char(char);
 int find_length(char *);
+char *copy_string(char *s);
 
 /* string_to_struct 
    allocates the memory for the objects in struct String;
@@ -23,7 +24,7 @@ struct String *string_to_struct(char *str) {
   }
 
   /* assign values for string and int initialized in String */
-  ptr -> str /*how does it know I mean the str inside the struct?*/ = str;
+  ptr -> str /*how does it know I mean the str inside the struct?*/ = copy_string(str, str_length);
 
   ptr -> length = str_length;
 
@@ -37,7 +38,24 @@ struct String *string_to_struct(char *str) {
 int find_length(char *s) {
   int i;
 
-  for (i = 0; s[i] != '\0'; i++) {}
+  for (i = 0; s[i] != '\0'; i++) {
+  }
 
   return i;
+}
+
+
+/* copy_string
+   allocates memory for a copy of the string;
+   and loops through the string to copy chars
+   to new string */
+char *copy_string(char *s, int length) {
+  char *copy;
+  int i;
+  
+  copy = malloc(sizeof(char)*length);
+  for (i = 0; i <= length; i++) {
+    copy[i] = s[i];
+  }
+  return copy;
 }
