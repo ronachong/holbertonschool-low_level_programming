1c48ad8a4d3b369a7b8c58cf68d835e36caac624
Rona Chong <ronachong@gmail.com>
2016-05-07 05:30:44 +0000

    Add printf statements to check that values of list change & print the string inside first node.

--- linked_lists/0-main.c
+++ linked_lists/0-main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
 
@@ -7,10 +8,22 @@ int main(void)
 {
   List *list;
 
+  printf("'list' is a pointer to the 'first' node in a list.\n");
   list = NULL;
+  printf("The value of 'list', i.e. the addr to the 'first' node, is %p\n", (void *)list);
+
+  printf("\nRunning add_node with 'Holberton' as string input.\n");
   if (add_node(&list, "Holberton") == 1)
     return (1);
+  printf("The value of 'list', i.e. the addr to the 'first' node, is now %p.\n", (void *)list);
+  printf("The string inside the 'first' node is %s.\n", list->str);
+
+
+  printf("\nRunning add_node with 'School' as string input.\n");
   if (add_node(&list, "School") == 1)
     return (1);
+  printf("The value of 'list', i.e. the addr to the 'first' node, is now %p.\n", (void *)list);
+  printf("The string inside the 'first' node is %s.\n", list->str);
+
   return (0);
 }
