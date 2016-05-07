8819a5e9d1f6f069fba8fa18234de2c8214a8600
Rona Chong <ronachong@gmail.com>
2016-05-07 23:55:39 +0000

    Add printf statements as tests in main code for Task 1.

--- linked_lists/8-main.c
+++ linked_lists/8-main.c
@@ -1,16 +1,36 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
 
 int add_node(List **list, char *str);
+struct List *find_end_of_list(struct List **);
 
 int main(void)
 {
   List *list;
+  struct List *ptr_to_current_tail;
 
+  printf("'list' is a pointer to the 'first' node in a list.\n");
   list = NULL;
+  printf("'list', i.e. the ptr to the 'first' node, is %p\n", (void *)list);
+
+  printf("\nRunning add_node with 'Holberton' as string input.\n");
   if (add_node(&list, "Holberton") == 1)
     return (1);
+  printf("'list', i.e. the ptr to the 'first' node, is now %p.\n", (void *)list);
+  printf("The string in the head of the list is %s\n", list->str);
+  ptr_to_current_tail = find_end_of_list(&list);
+  printf("ptr_to_current_tail is %p\n", (void *)ptr_to_current_tail);
+  printf("The string in the current tail of the list is %s\n", ptr_to_current_tail->str);
+
+  printf("\nRunning add_node with 'School' as string input.\n");
   if (add_node(&list, "School") == 1)
     return (1);
+  printf("'list', i.e. the ptr to the 'first' node, is still %p.\n", (void *)list);
+  printf("The string in the head of the list is still %s\n", list->str);
+  ptr_to_current_tail = find_end_of_list(&list);
+  printf("ptr_to_current_tail is now %p\n", (void *)ptr_to_current_tail);
+  printf("The string in the current tail of the list is %s\n", ptr_to_current_tail->str);
+
   return (0);
 }
