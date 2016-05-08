300ed9a7257d9c66038a1adb5f09c2b891ed6a9e
Rona Chong <ronachong@gmail.com>
2016-05-08 03:17:03 +0000

    Copy print_every_string_in_list to file for print_list.c.

---
+++ linked_lists/3-print_list.c
@@ -0,0 +1,15 @@
+/* print_every_string_in_list loops thru the list pointed to by                                             
+   ptr_to_head_of_list and prints the string in each node */
+void print_every_string_in_list(struct List *ptr_to_head_of_list) {
+  struct List *ptr_to_node;
+
+  /* initialize ptr to node as ptr to the head of the list */
+  ptr_to_node = ptr_to_head_of_list;
+
+  while (ptr_to_node->next != NULL) {
+    printf("%s\n", ptr_to_node->str);
+    ptr_to_node = ptr_to_node->next;
+  }
+
+  printf("%s\n", ptr_to_node->str);
+}
