5fa450a99e1c23dc59b726e670d777d7fc7a5460
Rona Chong <ronachong@gmail.com>
2016-05-08 03:12:46 +0000

    Correct/add missing components needed to make list_size work: change ptr_to_head to list, add declaration for i, include stdlib.h.

--- linked_lists/2-list_size.c
+++ linked_lists/2-list_size.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "list.h"
 
 /* print_every_string_in_list loops thru the list pointed to by                                             
@@ -5,10 +6,11 @@
    size of the list
 */
 int list_size(List *list) {
+  int i;
   struct List *ptr_to_node;
 
   /* initialize ptr to node as ptr to the head of the list */
-  ptr_to_node = ptr_to_head_of_list;
+  ptr_to_node = list;
 
   /* count up each time a node does not point to NULL */
   for (i = 0; ptr_to_node->next != NULL; i++) {
