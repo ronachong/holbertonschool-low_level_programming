f98b8a71dc05b87ec5e9fedc070ac29af219f644
Rona Chong <ronachong@gmail.com>
2016-05-08 02:21:31 +0000

    Revise params_to_list description for clarity.

--- linked_lists/1-params_to_list.c
+++ linked_lists/1-params_to_list.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "list.h"
 
+int add_node(List **, char *);
+char *copy_string(char *);
+int find_length(char *);
+struct List *find_end_of_list(struct List **);
+
 /* params_to_list takes in an array of string arguments /av/,
    creates a list with a node for each string arg,
    and returns the pointer to the first node of the list.
@@ -9,7 +14,7 @@
    A more detailed breakdown:
    params_to_list
    1) creates a pointer for the head of the list & makes this equal to NULL (temporarily);
-   2) then:
+   2) then, calls add_node (which:
       - uses malloc to allocate space for a new node and create a pointer to the memory;
       - uses malloc to allocate space for a copy of the string arg;
       - uses copy_string to copy the char values of the string arg to the memory for the copy;
@@ -17,8 +22,8 @@
       - makes the next pointer of the previous node (i.e. the current tail of the list) point
         to the new node, using find_end_of_list
 	UNLESS the new node happens to be the only node for the list so far, in which case,
-	params_to_list changes the header pointer to point to the new node.
-      for every string element in /av/, using add_node;
+	params_to_list changes the header pointer to point to the new node.)
+      for every string element in /av/
    3) finally, params_to_list returns the head pointer.
 */
 
