07fabe93b922208548bb44cad02f0431704b60a0
Rona Chong <ronachong@gmail.com>
2016-05-08 03:22:39 +0000

    Use print_string & print_char instead of printf to print chars.

--- linked_lists/3-print_list.c
+++ linked_lists/3-print_list.c
@@ -1,3 +1,6 @@
+int print_char(char);
+void print_string(char *s);
+
 /* print_every_string_in_list loops thru the list pointed to by                                             
    ptr_to_head_of_list and prints the string in each node */
 void print_list(List *list) {
@@ -12,4 +15,14 @@ void print_list(List *list) {
   }
 
   printf("%s\n", ptr_to_node->str);
+}
+
+/* print_string loops thru string input and prints every
+   char using print_char */
+void print_string(char *s) {
+  int i;
+  
+  for (i = 0; s[i] != '\0'; i++) {
+    print_char(s[i]);
+  }
 }
