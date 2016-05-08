8655d6b552e5bdac7c0ced76fb33bccebe1dc52f
Rona Chong <ronachong@gmail.com>
2016-05-08 01:16:34 +0000

    Write temp code to clarify how cmd line args will work.

--- linked_lists/1-params_to_list.c
+++ linked_lists/1-params_to_list.c
@@ -1,5 +1,19 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include "list.h"
+
+/* params_to_list takes in a number of arguments /av/,
+   creates a list with a node for each string arg,
+   and returns the pointer to the first node of the list.
+*/
 
 List *params_to_list(int ac, char **av) {
-  ;
+  List *temp_result;
+
+  ac = ac;
+  temp_result = NULL;
+  printf("The first arg is '%s'. The second arg is %s\n", av[1], av[2]);
+
+  return temp_result;
+
 }
