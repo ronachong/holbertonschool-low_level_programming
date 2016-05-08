74d68e274446cab79bfaf49d5187736a58b0e421
Rona Chong <ronachong@gmail.com>
2016-05-08 04:37:21 +0000

    Add printf and list_size calls to test success of free_list.

--- linked_lists/4-main.c
+++ linked_lists/4-main.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
 
 int add_node(List **list, char *str);
 void free_list(List *list);
+int list_size(List *list);
+
 
 int main(void)
 {
@@ -13,6 +16,8 @@ int main(void)
     return (1);
   if (add_node(&list, "School") == 1)
     return (1);
+  printf("The size of the list is %i\n", list_size(list));
   free_list(list);
+  printf("The size of the list is %i\n", list_size(list));
   return (0);
 }
