4f6983b39758b763ba542d21fbdac25039ccdac9
Rona Chong <ronachong@gmail.com>
2016-05-09 04:27:04 +0000

    Implement algo for rev_list using current_node and prev_node vars.

---
+++ linked_lists/5-rev_list.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "list.h"
+
+/* rev_list takes in a pointer to the addr of the head of a list and
+   reverses the order of the list by making each node point to its
+   predecessor; and updating the pointer to the head to point to the
+   last node.
+*/
+
+void rev_list(List **list) {
+  List *prev_node_ptr;
+  List *current_node_ptr;
+
+  /* do nothing if list is empty */
+  if (*list = NULL) {
+    return;
+  }
+
+  /* else */
+  /* initialize the pointer to previous node to NULL
+     & current node to first node in list */
+  prev_node_ptr = NULL;
+  current_node_ptr = *list;
+
+  /* for every node till the last node */
+  while (current_node_ptr->next != NULL) {
+    /* make current node point to prior node */
+    current_node_ptr->next = prev_node_ptr;
+
+    /* update previous node to be current node
+       & current node to be next node */
+    prev_node_ptr = current_node_ptr;
+    current_node_ptr = current_node_ptr->next;
+  }
+
+  /* make last node point to prior node */
+  current_node_ptr->next = prev_node_ptr;  
+
+  /* change the pointer to the first node to point to
+     last node */
+  *list = current_node_ptr;
+}
