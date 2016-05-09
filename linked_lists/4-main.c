0d53ba64657190e80466b58759f66c8fb00057c7
Rona Chong <ronachong@gmail.com>
2016-05-09 01:58:43 +0000

    Add list_size definition to main file.

--- linked_lists/4-main.c
+++ linked_lists/4-main.c
@@ -23,3 +23,26 @@ int main(void)
   printf("The string of the head is %s\n", list->str);
   return (0);
 }
+
+/* list_size loops thru the list pointed to by list and
+   increments up for every node in list, returning the
+   size of the list                                                                                    
+*/
+int list_size(List *list) {
+  int i;
+  struct List *ptr_to_node;
+
+  /* initialize ptr to node as ptr to the head of the list */
+  ptr_to_node = list;
+
+  /* count up each time a node does not point to NULL */
+  for (i = 0; ptr_to_node->next != NULL; i++) {
+    ptr_to_node = ptr_to_node->next;
+  }
+
+  /* count last node */
+  i++;
+
+  /* return count */
+  return i;
+}
