92e389ea99805fb4137e4784948b95b534c8e954
Rona Chong <ronachong@gmail.com>
2016-05-09 05:23:13 +0000

    Revise prototype for find_node_prior to use ptr to list and not ptr to ptr to list.

--- linked_lists/6-insert_in_list.c
+++ linked_lists/6-insert_in_list.c
@@ -1,6 +1,6 @@
 #include "list.h"
 
-List *find_node_prior(List **, int);
+List *find_node_prior(List *, int);
 List *create_new_node(char *content, List*);
 
 int insert_in_list(List **list, char *content, int index) {
@@ -9,7 +9,7 @@ int insert_in_list(List **list, char *content, int index) {
   List *ptr_to_new_node;
 
   /* find the node prior to node we want to insert */
-  ptr_to_node_prior = find_node_prior(**list, index);
+  ptr_to_node_prior = find_node_prior(*list, index);
 
   /* store next value of node prior */
   next_to_assign = node_prior_ptr->next;
@@ -19,4 +19,8 @@ int insert_in_list(List **list, char *content, int index) {
 
   /* change the next val of node prior to pointer of new node */
   ptr_to_node_prior->next = ptr_to_new_node;
+}
+
+List *find_node_prior(List *ptr_to_list, int index) {
+  ;
 }
