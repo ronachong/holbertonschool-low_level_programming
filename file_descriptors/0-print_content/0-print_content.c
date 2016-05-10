7440e3090a9f932bb6f4da1ac316a3f865a57b58
Rona Chong <ronachong@gmail.com>
2016-05-10 01:56:08 +0000

    Write preliminary code for check_arguments.

--- file_descriptors/0-print_content/0-print_content.c
+++ file_descriptors/0-print_content/0-print_content.c
@@ -1,3 +1,6 @@
+int check_arguments(int);
+int print_string(char *c);
+
 /* print_content 
    checks if the number of arguments is right,
    then opens the file with RD permissions (assuming the file already exists),
@@ -8,10 +11,22 @@
 int print_content(int argc, char **argv) {
   char *(or buffer?) contents;
 
-  check_arguments(argc);
-  open(argv, other params);
-  contents = read(argv, other params);
-  write(contents, params including std output);
+  if (check_arguments(argc) == 0) {
+    return 0;
+  }
 
+  /* open(argv, other params);
+  contents = read(argv, other params);
+  write(contents, params including std output); */
+
+  return 1;
+}
+
+int check_arguments (int argc) {
+  if (argc != 2) {
+    return 0;
+  }
+  
+  /* else */
   return 1;
 }
