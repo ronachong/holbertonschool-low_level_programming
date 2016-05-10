8a2fec31b0c6f1872aabd0c74bde307261bab99d
Rona Chong <ronachong@gmail.com>
2016-05-10 02:10:01 +0000

    Create header file and move prototypes, include directives to header file (consolidate code).

---
+++ file_descriptors/0-print_content/0-header.h
@@ -0,0 +1,5 @@
+#include  <unistd.h>
+
+int print_content(int, char **);
+int check_arguments(int);
+int print_string(char *string);
--- file_descriptors/0-print_content/0-main.c
+++ file_descriptors/0-print_content/0-main.c
@@ -1,5 +1,3 @@
-int print_content(int, char **);
-
 /* main file takes in arguments (i.e. string for file name) and passes it along to
    print_content */
 
