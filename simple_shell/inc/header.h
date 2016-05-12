4b637928cc9e907c5399a6b4f3a25beeb76062c9
Rona Chong <ronachong@gmail.com>
2016-05-12 23:05:09 +0000

    Add new prototypes and needed libraries.

--- simple_shell/inc/header.h
+++ simple_shell/inc/header.h
@@ -1,7 +1,13 @@
 /* This file will contain library include directives & prototypes for the simple
    shell program */
 
+/* includes */
+#include <unistd.h> /* write */
+#include <stdio.h> /* perror */
+#include <stdlib.h> /* malloc, free */
+
+
 /* prototypes */
 int shell (void);
 int print_prompt(void);
-int read_std_input(void)
+char *read_line(const int fd);
