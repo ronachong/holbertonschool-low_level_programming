af93559501d864a05f6463f7d2b0ac02b0beeec7
Rona Chong <ronachong@gmail.com>
2016-05-12 21:37:31 +0000

    Create file for shell fxn; write pseudocode.

--- simple_shell/inc/header.h
+++ simple_shell/inc/header.h
@@ -1,2 +1,7 @@
 /* This file will contain library include directives & prototypes for the simple
    shell program */
+
+/* prototypes */
+int shell (void);
+int print_prompt(void);
+int read_std_input(void)
---
+++ simple_shell/src/shell.c
@@ -0,0 +1,27 @@
+/* This file will contain the shell function for a simple shell. */
+
+int shell(void)
+{
+  char **std_input;
+  pid_t pid;
+
+  print_prompt();
+  std_input = read_std_input();
+
+  pid = fork(?);
+  if (pid == -1) {
+    /* do error handling */;
+  }
+  if (pid == 0) {
+    /* execute cmd */;
+    /* potentially pass values to parent? */;
+    /* exit */;
+  }
+  else {
+    wait(addr_to_store_to);
+    /* recursive call to bring prompt back to ready? */;
+    /* handle ctrl + D to end shell; later on maybe accept a cmd to quit */;
+  }
+
+  /* return any values for success? or error? */
+}
