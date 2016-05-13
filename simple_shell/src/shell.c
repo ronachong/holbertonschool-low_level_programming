5408ec6decc041baba3c4f5ceab0ec82c2baa505
Rona Chong <ronachong@gmail.com>
2016-05-13 00:25:44 +0000

    Use get_argv to get arguments from std input.

--- simple_shell/src/shell.c
+++ simple_shell/src/shell.c
@@ -7,7 +7,7 @@ int shell(void)
   pid_t pid;
 
   print_prompt();
-  std_input = read_line(0);
+  argv = get_argv();
 
   pid = fork();
   if (pid == -1) {
@@ -24,6 +24,8 @@ int shell(void)
     /* recursive call to bring prompt back to ready? */;
     /* handle ctrl + D to end shell; later on maybe accept a cmd to quit */;
   }
+
+  /* free everything: including, argv pointer and strings inside */
 
   /* return any values for success? or error? */
 }
