d8502321afe41f1e0e5772788ec4705fd32ea844
Rona Chong <ronachong@gmail.com>
2016-05-15 23:53:34 +0000

    Use nested loops to read and open dirs.

---
+++ simple_shell/src/get_fp.c
@@ -0,0 +1,29 @@
+#include "header.h"
+
+/* 
+ * get_fp searches the list of directory paths provided to it and locates the
+ * first directory containing a file matching argv0.
+ */
+
+char *get_fp(char *argv0, char **paths) {
+  int i;
+  DIR *dir_p;
+  struct dirent *dir_ent_p;
+  
+  printf("This is a test.\n");
+
+  argv0 = argv0;
+  paths = paths;
+
+  for (i = 0; paths[i] != NULL; i++) {
+    printf("Iteration at %s\n", paths[i]);
+    dir_p = opendir(paths[i]);
+    dir_ent_p = readdir(dir_p);
+    while (dir_ent_p != NULL) {
+      dir_ent_p = readdir(dir_p);
+      if (dir_ent_p != NULL)
+	printf("%s\n", dir_ent_p->d_name);
+    }
+  }
+  return dir_ent_p->d_name;
+}
