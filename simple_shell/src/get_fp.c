709329a952edab30f9c723292c529a363e27b951
Rona Chong <ronachong@gmail.com>
2016-05-16 19:25:46 +0000

    Add error handling for opendir call; more printf tests.

--- simple_shell/src/get_fp.c
+++ simple_shell/src/get_fp.c
@@ -16,7 +16,13 @@ char *get_fp(char *argv0, char **paths) {
   printf("Running get_fp.\n");
   for (i = 0; paths[i] != NULL; i++) {
     dir_p = opendir(paths[i]);
+    if (dir_p == NULL) {
+      printf("the directory is %s\n", paths[i]);
+      perror("opendir(paths[i])");
+    }
+
     printf("Running while loop, loop #%i\n", i);
+    printf("dir_p is %p\n", (void *)dir_p);
     while ((dir_ent_p = readdir(dir_p)) != NULL) {
       if (string_comparison(argv0, dir_ent_p->d_name) == 1) {
 	tmp = concat_string(paths[i], "/");
