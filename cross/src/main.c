f7a36dcc23648088582fc826b34b61311d05ef0d
Rona Chong <ronachong@gmail.com>
2016-06-17 06:21:49 +0000

    Handle improper values passed to cross using str_to_int.
    Also update code commenting.

--- cross/src/main.c
+++ cross/src/main.c
@@ -3,17 +3,28 @@
 int main(int ac, char **av)
 {
   int n_rows;
-
+  
+  /* handle improper number of args */
   if (ac != 2) {
     print_string("Please pass one non-negative integer to the program.\n");
     return (1);
     }
 
   n_rows = str_to_int(av[1]);
+  /* handle improper values passed as argument */
+  if (n_rows == -1) {
+      print_string("Invalid argument. Please pass a non-negative integer to \
+the program.\n");
+      return (1);
+    }
+
   /* print first half of cross */
+
+  /* handle cases of odd n_rows */
   if (n_rows % 2 == 1) {
     print_char('X');
   }
+
   /* print second half of cross*/
   return (0);
 }
