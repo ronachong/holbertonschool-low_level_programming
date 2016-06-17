37838fc09b40e0341cee12c9bcdd4720e0fcd654
Rona Chong <ronachong@gmail.com>
2016-06-17 05:51:38 +0000

    Test find_length - function works.

--- cross/src/main.c
+++ cross/src/main.c
@@ -2,12 +2,14 @@
 
 int main(int ac, char **av)
 {
+  int n_rows;
+
   if (ac != 2) {
-    print_string("Please pass one (integer) argument to the program.\n");
+    print_string("Please pass one non-negative integer to the program.\n");
     return (1)
     }
 
-  n_rows = str_to_int(av[1])
+  n_rows = str_to_int(av[1]);
   /* print first half of cross */
   if (n_rows % 2 == 1) {
     print_char('X');
