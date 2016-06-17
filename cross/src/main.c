7cf14e15de0180bb6c4bf89b208646cd9bd31084
Rona Chong <ronachong@gmail.com>
2016-06-17 06:11:52 +0000

    Implement str_to_int and X printing for cross's main code.
    Update Makefile with new dependencies.

--- cross/src/main.c
+++ cross/src/main.c
@@ -6,13 +6,14 @@ int main(int ac, char **av)
 
   if (ac != 2) {
     print_string("Please pass one non-negative integer to the program.\n");
-    return (1)
+    return (1);
     }
 
   n_rows = str_to_int(av[1]);
   /* print first half of cross */
   if (n_rows % 2 == 1) {
     print_char('X');
+  }
   /* print second half of cross*/
   return (0);
 }
