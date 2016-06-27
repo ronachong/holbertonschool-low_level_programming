d86b12f2beffd435421e017401f6bc09f7700826
Rona Chong <ronachong@gmail.com>
2016-06-26 18:03:20 -0700

    Create preliminary variables for add_end_dl_list.

--- advanced_linked_lists/double_linked_list/add_to_dl_list.c
+++ advanced_linked_lists/double_linked_list/add_to_dl_list.c
@@ -1,3 +1,5 @@
+#include "list.h"
+
 /*
  * add_end_dl_list takes in a pointer to a pointer, list. The address which list
  * points to should be the first item in a linked list, or NULL. add_end_dl_list
@@ -6,9 +8,15 @@
  */
 
 int add_end_dl_list(List **list, char *str) {
+  List *node_ptr;
+  char *node_str;
+  List *next_ptr;
+  List *prev_ptr;
+  
   /* steps : */
   /* create space for node, as in: */
   /* malloc space for the node as a whole? */
+  
   /* malloc space for the str in the node?  */
   /* malloc space for the prev pointer in the node? */
   /* malloc space for the next pointer in the node? */
