15323b7950c4f546472f74331d722384e7c1bf55
Rona Chong <ronachong@gmail.com>
2016-06-26 18:45:56 -0700

    Remove malloc code for node string in add_end_dl_list.
    add_end_dl_list will use strdup to create the string, which
    mallocs space for the string on its own.

--- advanced_linked_lists/double_linked_list/add_to_dl_list.c
+++ advanced_linked_lists/double_linked_list/add_to_dl_list.c
@@ -13,22 +13,13 @@ int add_end_dl_list(List **list, char *str) {
   List *next_ptr;
   List *prev_ptr;
   
-  /* steps : */
-  /* create space for node, as in: */
-  /* malloc space for the node,
-     including the elements housed by node (ptr values) */
+  /* malloc space for node, including elements housed by node (ptr values) */
   node_ptr = malloc(sizeof(List));
   /* return 1 if malloc fails */
   if (node_ptr == NULL) {
     return 1;
   }
-  /* malloc space for chars in string pointed to by str in node.
-     not sure if strdup already does this */
-  node_str = malloc(sizeof(char)*str_length);
-  /* return 1 if malloc fails */
-  if (node_str == NULL) {
-    return 1;
-  }
+
  
   /* assign values for node, as in */
   /* use strdup to copy str, and make node str equal to copy */
