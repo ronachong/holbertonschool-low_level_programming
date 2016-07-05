8548f470da56900d35c8f0052608ec236035c86a
Rona Chong <ronachong@gmail.com>
2016-07-05 23:52:45 +0000

    Insert preliminary code for ht_create.

--- hash_tables/ht_create.c
+++ hash_tables/ht_create.c
@@ -8,9 +8,15 @@
 
 HashTable *ht_create(unsigned int size)
 {
+  HashTable *ht_ptr;
+  List **array;
+
   /* create HashTable struct using malloc */
+  ht_ptr = malloc(sizeof(HashTable));
   /* create array for HashTable using malloc */
+  array = malloc(sizeof(List *)*size);
   /* assign value for size element of HashTable */
+  ht_ptr->size = size;
   /* assign array to HashTable */
-  ;
+  ht_ptr->array = array;
 }
