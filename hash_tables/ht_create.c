04778cadce5962e0228ed0c6987bcd2fd67006d0
Rona Chong <ronachong@gmail.com>
2016-07-05 23:36:30 +0000

    Start file for ht_create with description and pseudocode.
    Malloc allocates memory for the hash table and its array and re-
    turns pointers to each. The memory will remain for each data
    structure until freed.
    
    The returned pointers allow for the assignment of the array to
    the hash table and the return of the hash table.

---
+++ hash_tables/ht_create.c
@@ -0,0 +1,16 @@
+#include "hashtable.h"
+
+/*
+ * ht_create takes in the unsigned int "size" for its parameter and returns
+ * a pointer to a HashTable struct with an array containing "size" cells.
+ * In essence, ht_create creates a hash table of size "size."
+ */
+
+HashTable *ht_create(unsigned int size)
+{
+  /* create HashTable struct using malloc */
+  /* create array for HashTable using malloc */
+  /* assign value for size element of HashTable */
+  /* assign array to HashTable */
+  ;
+}
