f5daecaaf149e9bad17a8f135401c7ee6e07c9d5
Rona Chong <ronachong@gmail.com>
2016-07-06 00:18:23 +0000

    Start ht_put with description and pseudocode.

---
+++ hash_tables/ht_put.c
@@ -0,0 +1,21 @@
+#include "hashtable.h"
+
+/*
+ * ht_put takes in the following three parameters:
+ * 1) the pointer @hashtable, pointing to a hash table
+ * 2) the string @key, to be hashed
+ * 3) the string @value, which is the value corresponding to key
+ *
+ * ht_put hashes @key, creates and adds a linked list node containing @key and
+ * @value to the hash table pointed to by @hashtable, and returns an integer
+ * representing the index of the key and value pair in the hash table.
+ */
+
+int ht_put(HashTable *hashtable, const char *key, const char *value)
+{
+  /* hash @key and get index, using hash */
+  /* create linked list node with @key and @value */
+  /* add new node to hash table at index */
+  /* return index of key and value pair in hash table */
+  ;
+}
