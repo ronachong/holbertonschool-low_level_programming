cf46b8e912ff724e90a2ee05788bc163a000e06c
Rona Chong <ronachong@gmail.com>
2016-07-06 03:07:30 +0000

    Write preliminary iteration of ht_get.

--- hash_tables/ht_get.c
+++ hash_tables/ht_get.c
@@ -4,14 +4,25 @@
  * ht_get takes in the following parameters:
  * 1) a pointer to a hash table @hashtable
  * 2) a string representing a key stored in the hash table, @key
+ *
  * ht_get retrieves the index of @key in @hashtable and returns the value corres-
  * ponding to @key within the hash table.
  */
 char *ht_get(HashTable *hashtable, const char *key)
 {
+  int i;
+  List *n_ptr;
+  char *value;
+
   /* hash @key to get index, using hash */
+  i = hash(key, sizeof(hashtable));
   /* find node in hash table list with @key as key value, using index */
-  /* find corresponding value */
-  /* return value */
-  ;
+  n_ptr = find_node_in_ht(hashtable, key);
+  if (n_ptr == NULL)
+    return NULL;
+  /* else: */
+  /* return corresponding value */
+  return n_ptr->value;
 }
+
+
