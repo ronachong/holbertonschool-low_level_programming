a009499f0b530499a5109dedde596b4aa46fdef4
Rona Chong <ronachong@gmail.com>
2016-07-06 03:00:17 +0000

    Start ht_get with description and pseudocode.

---
+++ hash_tables/ht_get.c
@@ -0,0 +1,17 @@
+#include "hashtable.h"
+
+/*
+ * ht_get takes in the following parameters:
+ * 1) a pointer to a hash table @hashtable
+ * 2) a string representing a key stored in the hash table, @key
+ * ht_get retrieves the index of @key in @hashtable and returns the value corres-
+ * ponding to @key within the hash table.
+ */
+char *ht_get(HashTable *hashtable, const char *key)
+{
+  /* hash @key to get index, using hash */
+  /* find node in hash table list with @key as key value, using index */
+  /* find corresponding value */
+  /* return value */
+  ;
+}
