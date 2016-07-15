e3d762cf8581bf31f887790d14c1cc043123b7f6
Rona Chong <ronachong@gmail.com>
2016-07-15 14:15:29 +0000

    Start bubble_sort with description and pseudocode.

---
+++ sorting_algorithms/bubble_sort.c
@@ -0,0 +1,17 @@
+/*
+ * bubble_sort takes in an int array, @array, and bubble-sorts the given array
+ * into ascending order.
+ */
+
+void bubble_sort(int *array, int size)
+{
+  int i
+  int rounds to sort
+  int indices to sort;
+  
+  for (i = 0; i < rounds; i++, indices--) {
+    for (i = 0, i < indices, i++) {
+      switch array[i] with array[i + 1] if array[i] > array[i+1];
+    }
+  }
+}
