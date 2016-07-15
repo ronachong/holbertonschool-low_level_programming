48f8ef6fced394816a57c826be774315cc9bf0a2
Rona Chong <ronachong@gmail.com>
2016-07-15 14:31:36 +0000

    Write preliminary iteration of bubble_sort.

--- sorting_algorithms/bubble_sort.c
+++ sorting_algorithms/bubble_sort.c
@@ -5,13 +5,16 @@
 
 void bubble_sort(int *array, int size)
 {
-  int i
-  int rounds to sort
-  int indices to sort;
-  
-  for (i = 0; i < rounds; i++, indices--) {
+  int i;
+  int indices;
+  int tmp;
+
+  for (i = 0; i < size - 1; i++, indices--) {
     for (i = 0, i < indices, i++) {
-      switch array[i] with array[i + 1] if array[i] > array[i+1];
+      if (array[1] > array[i + 1]) {
+	tmp = array[i];
+	array[i] = array[i + 1];
+	array[i + 1] = tmp;
     }
   }
 }
