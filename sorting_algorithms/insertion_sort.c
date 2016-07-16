1574314d4cf88f3b8ea0553a6c1c742f6cfa6c3d
Rona Chong <ronachong@gmail.com>
2016-07-16 04:38:10 +0000

    Remove test print statements in insertion_sort. (no longer needed)

--- sorting_algorithms/insertion_sort.c
+++ sorting_algorithms/insertion_sort.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
-
 /*
  * insertion_sort takes in an int array, @array, and an int representing
  * the size of the given array, @size, for its parameters.
- * insertion_sort sorts the given array via an insertion sort.
+ * insertion_sort sorts the given array into ascending order via an insertion
+ * sort.
  */
 
 void insertion_sort(int *array, int size)
@@ -14,13 +13,11 @@ void insertion_sort(int *array, int size)
 
   for (ind = 0; ind < size; ind++) {
     for (i = ind; i > 0; i--) {
-      printf("checking a[%d] with a[%d]\n", i, i - 1);
       if (array[i] < array[i - 1]) {
     	tmp = array[i];
     	array[i] = array[i - 1];
     	array[i - 1] = tmp;
       }
     }
-    printf("\n");
   }
 }
