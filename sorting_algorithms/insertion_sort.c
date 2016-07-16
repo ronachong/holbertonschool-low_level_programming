5fa985675df11dc0ab5578b2ae4f1a7b096c9f2c
Rona Chong <ronachong@gmail.com>
2016-07-16 04:25:47 +0000

    Make fixes to insertion_sort regarding @ind. Test w/ printf.
    Fix algo for insertion_sort by initializing @ind at end of array;
    also decrementing @ind at the end of each round.

--- sorting_algorithms/insertion_sort.c
+++ sorting_algorithms/insertion_sort.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /*
  * insertion_sort takes in an int array, @array, and an int representing
  * the size of the given array, @size, for its parameters.
@@ -9,17 +11,21 @@ void insertion_sort(int *array, int size)
   int r;
   int i;
   int ind;
-  int tmp;
+  /* int tmp; */
 
-  for (r = 0; r < size; r++, ind++) {
-    /* in each round, sort up to i many indices in the array,
-       i.e., compare each item in the array prior to array[ind] with array[ind] */
+  array = array;
+  /* tmp = 0; */
+
+
+    for (r = 0, ind = size - 1; r < size; r++, ind--) {
     for (i = ind; i > 0; i--) {
-      if (array[i] < array[i - 1]) {
-	tmp = array[i];
-	array[i] = array[i - 1];
-	array[i - 1] = tmp;
-      }
+      printf("checking a[%d] with a[%d]\n", i, i - 1);
+    /*   if (array[i] < array[i - 1]) { */
+    /* 	tmp = array[i]; */
+    /* 	array[i] = array[i - 1]; */
+    /* 	array[i - 1] = tmp; */
+    /*   } */
     }
+    printf("\n");
   }
 }
