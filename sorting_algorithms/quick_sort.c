25ec3063f1d603a6dc9e7edcd054a684f7d3c176
Rona Chong <ronachong@gmail.com>
2016-07-16 05:44:38 +0000

    Write descriptions for quick_sort and partition functions.
    Also write verbal pseudocode for partition.

---
+++ sorting_algorithms/quick_sort.c
@@ -0,0 +1,36 @@
+/*
+ * quick_sort takes in an int array @array, and an int representing the size of
+ * the given array, @size.
+ * quick_sort sorts the given array into ascending order via quick sort.
+ */
+
+void quick_sort(int *array, int size)
+{
+  ;
+}
+
+/* 
+ * partition takes in the following parameters:
+ * - int array @array
+ * - int representing size of @array, @size
+ * - int representing pivot value for partition, @pivot 
+ * partition arranges the given array such that the first half of @array con-
+ * tains values less than the pivot, and the second half contains values greater
+ * than the pivot.
+ */
+void partition(int *array, int size, int pivot)
+{
+  s1;
+  s2;
+
+  /* start with s1 at beginning of array (val = 0) */
+  /* start with s2 at end of array (val = size) */
+
+  /* check if value at s1 is equal to or greater than pivot */
+  /* if not, increment s1, till it is */
+  /* check if value at s2 is equal to or less than pivot */
+  /* if not, increment s2, till it is */
+  /* swap values at s1 and s2 */
+  /* repeat */
+  /* end when the value at s1 and s2 are both equal to the pivot */
+}
