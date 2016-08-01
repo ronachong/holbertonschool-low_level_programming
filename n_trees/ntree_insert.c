6271e17f682d33f823fb9617bd06497474607a05
Rona Chong <ronachong@gmail.com>
2016-07-31 17:48:24 -0700

    Amend pseudocode; write helper function create_ntnode for insert.
    Initialize children and next elements as NULL instead of pointers
    to NULL, in pseudocode. Saves trouble of dereferencing pointers
    and follows example of tree = NULL in main code provided by Alex.
    
    Write preliminary create_ntnode function per pseudocode.

--- n_trees/ntree_insert.c
+++ n_trees/ntree_insert.c
@@ -1,8 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+
+int ntree_insert(NTree **tree, char **parents, char *data)
+{
+  ;
+}
+
+/**
+ * create_ntnode creates a new NTree node and returns the pointer to it.
+ */
+NTree *create_ntnode(char *data)
+{
+  NTree *new_node;
+
+  new_node = malloc(sizeof(NTree));
+  new_node->str = strdup(data);
+  new_node->children = NULL;
+
+  return new_node;
+}
+
 /*
 make new ntree node:
 malloc node and assign pointer to @new_node
 assign strdrup(@data) to @new_node->str
-assign pointer to NULL to @new_node->children
+assign NULL to @new_node->children
 
 insert @new_node:  
  if @parents is NULL or [NULL]--
@@ -12,15 +34,14 @@ insert @new_node:
   create new linked list node:
    malloc list and assign pointer to @new_list
    assign @new_node to @new_list->node
-   assign pointer to NULL to @new_list->next
+   assign NULL to @new_list->next
 
   find parent node:
    traverse tree till reaching last parent - node with str=string_n
    set last parent node to @parent_node
 
   insert @new_list:
-   if @parent_node has children (@parent_node->children is not a pointer to NUL
-   L:
+   if @parent_node has children (@parent_node->children != NULL):
     store @parent_node->children in new_list->next
     
    make @parent_node->children point to @new_node (@parent_node->children = @new_node)
