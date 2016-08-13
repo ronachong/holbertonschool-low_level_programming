4caa09d237de62b4715fcd5bf4acd132bbd7a10a
Rona Chong <ronachong@gmail.com>
2016-08-13 16:55:42 -0700

    Update pseudocode for path_exists; eliminate depth variable.
    i serves the same purpose of representing the depth of the tree
    being checked, so @depth is a redundant variable.

--- n_trees/path_exists.c
+++ n_trees/path_exists.c
@@ -1,12 +1,24 @@
-start at depth 0
-start node to check at tree
+int path_exists(NTree *tree, char **path)
+{
+
+	while path[i] != NULL:
+		/* check if path[i] exists in tree at given depth */
+		if (depth == 0 &&
+}
+
+/*
+start node to check at tree (i.e. depth 0)
 
 while path[i] != NULL:
 	check if path[i] is in a node at given depth:
-		if depth is at 0, simply check node->str:
+		if i/depth is at 0, simply check node->str:
 			return 0 if node->str != path[i]
 		else check each node in linked list:
 			return 0 if none of the nodes have a str == path[i]
 
+	update node to children of current node (increase depth)
+	update i
+
 loops were successfully completed (meaning a match for each path[i] was found):
 return 1
+*/
